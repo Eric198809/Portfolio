c59f655a4e2a8cddc9392dfa462d304f1c71a79e
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-06-18 11:36:01 +0200

    MAJ

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./style/setting.scss";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Outlet />
       </main>
+      <Footer/>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,9 @@ import logoGithub2 from "../assets/github2.svg";
 import logoReact from "../assets/react.svg";
 import logoWorld from "../assets/siteprojet.svg";
 
+import arrowUp from "../assets/arrowup.svg";
+
+
 import "../style/home.scss";
 import { useState, useEffect } from "react";
 
@@ -32,68 +35,115 @@ function Home() {
     // Cleanup interval on component unmount
     return () => clearInterval(interval);
   }, [wordsDescription]);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <header>
-        <div className="title_image_header">
+        <div className="arrow_up" onClick={scrollToTop}>
+          <img src={arrowUp} alt="" />
+        </div>
+        <div className="header-container">
+          <p>Bonjour, je m'appelle </p>
           <h1 className="header_title">
-            Hello ,<br />
-            I’m Eric Besson <br />
-            Web developper <br />
-            {currentWord}
+            Eric Besson <br />
+            Développeur web {currentWord}
+            <br />
           </h1>
-          <img src={imageheader} alt="image-header" />
+          <p>
+            Je suis Eric, Développeur Web & Mobile, passionné par l'univers du
+            code et des nouvelles technologies.<br></br> Bienvenue sur mon
+            portfolio.
+          </p>
         </div>
         <div className="header-buttons">
-          <button className="header_button">
+          <a
+            href="https://www.linkedin.com/in/eric-besson-517763123/"
+            className="header_button"
+          >
             Linkedin
             <img src={logoLinkedin} alt="logo Linkedin" />
-          </button>
-          <button className="header_button">
+          </a>
+          <a href="https://github.com/Eric198809" className="header_button">
             Github
             <img src={logoGithub} alt="logo Github" />
-          </button>
+          </a>
         </div>
       </header>
       <section className="section_container">
         <div className="title_section">
-          <h2>SKILLS</h2>
+          <h2>COMPETENCES</h2>
           <div className="line"></div>
         </div>
-        <div className="content_skills">
-          <h3 className="title_skills">Front-end</h3>
-          <div className="skills_images">
+        <div className="skill_container">
+          <h3 className="title_skill">FRONT-END</h3>
+          <div className="skill_image">
             <img src={logoReact} alt="react" />
-            <img src={logoJavascript} alt="javascript" />
-            <img src={logoSass} alt="sass" />
-            <img src={logoHtml} alt="html" />
-            <img src={logoCss} alt="css" />
-            <img src={logoWordpress} alt="wordrepss" />
+            <p>React</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoJavascript} alt="react" />
+            <p>Javascript</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoSass} alt="react" />
+            <p>Sass</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoHtml} alt="react" />
+            <p>HTML</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoCss} alt="react" />
+            <p>CSS</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoWordpress} alt="react" />
+            <p>Wordpress</p>
           </div>
         </div>
-        <div className="content_skills">
-          {" "}
-          <h3 className="title_skills">Back-end</h3>
-          <div className="skills_images">
-            <img src={logoNode} alt="node" />
-            <img src={logoExpress} alt="express" />
-            <img src={logoSql} alt="sql" />
+        <div className="skill_container">
+          <h3 className="title_skill">BACK-END</h3>
+          <div className="skill_image">
+            <img src={logoNode} alt="react" />
+            <p>Node JS</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoExpress} alt="react" />
+            <p>Express</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoSql} alt="react" />
+            <p>SQL</p>
           </div>
         </div>
-        <div className="content_skills">
-          {" "}
-          <h3 className="title_skills">Design</h3>
-          <div className="skills_images">
-            <img src={logoFigma} alt="figma" />
+        <div className="skill_container">
+          <h3 className="title_skill">DESIGN</h3>
+          <div className="skill_image">
+            <img src={logoFigma} alt="react" />
+            <p>Figma</p>
           </div>
         </div>
-        <div className="content_skills">
-          {" "}
-          <h3 className="title_skills">Outils</h3>
-          <div className="skills_images">
-            <img src={logoTrello} alt="trello" />
-            <img src={logoGit} alt="git" />
-            <img src={logoGithub2} alt="github" />
+        <div className="skill_container">
+          <h3 className="title_skill">OUTILS</h3>
+
+          <div className="skill_image">
+            <img src={logoTrello} alt="react" />
+            <p>Trello</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoGit} alt="react" />
+            <p>Git</p>
+          </div>
+          <div className="skill_image">
+            <img src={logoGithub2} alt="react" />
+            <p>Github</p>
           </div>
         </div>
       </section>
@@ -102,73 +152,114 @@ function Home() {
           <h2>PROJETS</h2>
           <div className="line"></div>
         </div>
-        <div className="projet_container">
+        <div className="projets_container">
+          <div className="content-projet">
+            <h3>LE COMPTOIR DES SEELIES</h3>
+            <h4>Détails</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+              aperiam, nostrum ratione voluptas debitis saepe odit, omnis
+              dolorum nihil, iste nam impedit vel quisquam! Consequuntur rem
+              magni delectus nisi cum. Iusto voluptatem asperiores itaque
+              reiciendis enim sunt impedit modi maiores voluptatibus quo, dicta
+              dolor cumque distinctio nisi quos fuga recusandae perferendis odit
+              consequatur soluta qui nemo officia tempore aliquam. Porro.
+            </p>
+            <h4>Stack</h4>
+            <div className="images_stacks">
+              <img className="logo_stack" src={logoReact} alt="react" />
+              <img className="logo_stack" src={logoNode} alt="node" />
+              <img className="logo_stack" src={logoSass} alt="sass" />
+              <img className="logo_stack" src={logoExpress} alt="express" />
+              <img className="logo_stack" src={logoSql} alt="sql" />
+            </div>
+            <div className="header-buttons">
+              <a
+                href="https://github.com/WildCodeSchool/11-2023-Rem-FR-Crew-Bleu-LaPromodoro-G2-P3/"
+                className="header_button"
+              >
+                Voir le Github
+                <img src={logoGithub} alt="logo Github" />
+              </a>
+            </div>
+          </div>
           <div className="image_projet"></div>
-          <h3>LE COMPTOIR DES SEELIES</h3>
-          <div className="détails_projet">
-            <div className="résumé">
-              <h4>Détails</h4>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Sapiente aperiam, nostrum ratione voluptas debitis saepe odit,
-                omnis dolorum nihil, iste nam impedit vel quisquam! Consequuntur
-                rem magni delectus nisi cum. Iusto voluptatem asperiores itaque
-                reiciendis enim sunt impedit modi maiores voluptatibus quo,
-                dicta dolor cumque distinctio nisi quos fuga recusandae
-                perferendis odit consequatur soluta qui nemo officia tempore
-                aliquam. Porro.
-              </p>
-            </div>
-            <div className="stack_projet">
-              <h4>Stack</h4>
-              <div className="images_stacks">
-                <img className="logo_stack" src={logoHtml} alt="" />
-                <img className="logo_stack" src={logoCss} alt="" />
-              </div>
-            </div>
-          </div>
-          <div className="header-buttons">
-            <button className="header_button">
-              Voir le Github
-              <img src={logoGithub} alt="logo Github" />
-            </button>
-          </div>
         </div>
-        <div className="projet_container">
+        <div className="projets_container">
+          <div className="content-projet">
+            <h3>GENKO</h3>
+            <h4>Détails</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+              aperiam, nostrum ratione voluptas debitis saepe odit, omnis
+              dolorum nihil, iste nam impedit vel quisquam! Consequuntur rem
+              magni delectus nisi cum. Iusto voluptatem asperiores itaque
+              reiciendis enim sunt impedit modi maiores voluptatibus quo, dicta
+              dolor cumque distinctio nisi quos fuga recusandae perferendis odit
+              consequatur soluta qui nemo officia tempore aliquam. Porro.
+            </p>
+            <h4>Stack</h4>
+            <div className="images_stacks">
+              <img className="logo_stack" src={logoReact} alt="react" />
+              <img className="logo_stack" src={logoCss} alt="css" />
+              <img className="logo_stack" src={logoNode} alt="node" />
+            </div>
+            <div className="header-buttons">
+              <a
+                href="https://github.com/WildCodeSchool/11-2023-Rem-FR-Crew-Bleu-LaPromodoro-G4"
+                className="header_button"
+              >
+                Voir le Github
+                <img src={logoGithub} alt="logo Github" />
+              </a>
+              <a
+                href="https://main--chipper-youtiao-cca0a4.netlify.app/"
+                className="header_button"
+              >
+                Voir le site
+                <img src={logoWorld} alt="logo world" />
+              </a>
+            </div>
+          </div>
           <div className="image_projet2"></div>
-          <h3>GENKO</h3>
-          <div className="détails_projet">
-            <div className="résumé">
-              <h4>Détails</h4>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Sapiente aperiam, nostrum ratione voluptas debitis saepe odit,
-                omnis dolorum nihil, iste nam impedit vel quisquam! Consequuntur
-                rem magni delectus nisi cum. Iusto voluptatem asperiores itaque
-                reiciendis enim sunt impedit modi maiores voluptatibus quo,
-                dicta dolor cumque distinctio nisi quos fuga recusandae
-                perferendis odit consequatur soluta qui nemo officia tempore
-                aliquam. Porro.
-              </p>
+        </div>
+        <div className="projets_container">
+          <div className="content-projet">
+            <h3>QUE L'AMOUR SOIT AVEC TOI</h3>
+            <h4>Détails</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente
+              aperiam, nostrum ratione voluptas debitis saepe odit, omnis
+              dolorum nihil, iste nam impedit vel quisquam! Consequuntur rem
+              magni delectus nisi cum. Iusto voluptatem asperiores itaque
+              reiciendis enim sunt impedit modi maiores voluptatibus quo, dicta
+              dolor cumque distinctio nisi quos fuga recusandae perferendis odit
+              consequatur soluta qui nemo officia tempore aliquam. Porro.
+            </p>
+            <h4>Stack</h4>
+            <div className="images_stacks">
+              <img className="logo_stack" src={logoReact} alt="react" />
+              <img className="logo_stack" src={logoCss} alt="css" />
+              <img className="logo_stack" src={logoNode} alt="node" />
             </div>
-            <div className="stack_projet">
-              <h4>Stack</h4>
-              <div className="images_stacks">
-                <img className="logo_stack" src={logoHtml} alt="" />
-                <img className="logo_stack" src={logoCss} alt="" />
-              </div>
+            <div className="header-buttons">
+              <a
+                href="https://github.com/Eric198809/hackathon"
+                className="header_button"
+              >
+                Voir le Github
+                <img src={logoGithub} alt="logo Github" />
+              </a>
+              <a
+                href="https://paul-lac.github.io/QueLAmourSoitAvecToi/"
+                className="header_button"
+              >
+                Voir le site
+                <img src={logoWorld} alt="logo world" />
+              </a>
             </div>
           </div>
-          <div className="header-buttons">
-            <button className="header_button">
-              Voir le Github
-              <img src={logoGithub} alt="logo Github" />
-            </button>
-            <button className="header_button">
-              Voir le site
-              <img src={logoWorld} alt="logo world" />
-            </button>
-          </div>
+          <div className="image_projet3"></div>
         </div>
       </section>
     </>
